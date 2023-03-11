97589b63307f44014ada50c0ff39a2bb19d41f93
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-03-11 10:08:26 -0500

    Uploading to git

---
+++ app.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form'); 
+const createMeme = document.querySelector('.createMeme');
+const imgUrl = document.querySelector('#imageURL');
+const topText = document.querySelector('#topText');
+const bottomText = document.querySelector('#bottomText');
+
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let memeDiv  = document.createElement('div');
+    memeDiv.classList.add('memes');
+
+    let imgTopText = document.createElement('div');
+    imgTopText.classList.add('top-center');
+    imgTopText.innerHTML= topText.value.toUpperCase();
+
+    memeDiv.style.backgroundImage= "url(" + imgUrl.value + ")";
+
+    let imgBottomText= document.createElement('div');
+    imgBottomText.classList.add('bottom-center');
+    imgBottomText.innerText = bottomText.value.toUpperCase();
+
+    memeDiv.append(imgTopText, imgBottomText);
+    memeDiv.addEventListener('click', handleClick)
+   
+    // add the meme div to the createMeme div
+    createMeme.append(memeDiv);
+    
+    
+})
+
+function handleClick(){
+    this.remove()
+}
